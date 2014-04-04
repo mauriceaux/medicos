c2cfd16814c862ff179a1e68c5025bb3bd6064cd
ccoottee <malopezdaza@gmail.com>
2014-04-04 00:05:54 -0400

    segundo commit

---
+++ medicos/app/Controller/ComunasController.php
@@ -0,0 +1,5 @@
+<?php
+class ComunasController extends AppController {
+    var $scaffold;
+}
+?>
---
+++ medicos/app/Model/comuna.php
@@ -0,0 +1,10 @@
+<?php
+
+	class comunas extends AppModel
+	
+	{
+		//var $hasMany = array ('coments');
+		//ver la referencia
+		
+	}
+?>
